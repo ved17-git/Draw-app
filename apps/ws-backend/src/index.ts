c003fc78494adb0c7978725824f6db7df43088a2
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2025-08-14 22:49:51 +0530

    added rooms

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -5,35 +5,67 @@ import { verifyToken } from './verify';
 const port=8001
 const wss=new WebSocketServer({port:port})
 
+interface typeLocalDb{
+    userId:number,
+    rooms:number[]
+    socket:WebSocket
+}
+
+    
+const localDB:typeLocalDb[]=[]
+
 wss.on("connection",(socket, req)=>{
 
-    const url=req.url
+    const url=req.url  //url=/join?token=abc
     if(!url){
     return
     }
-    const t=url.split('?')[1];
-    const params=new URLSearchParams(t);
+    const t=url.split('?')[1];  //url=["/join","token=abc"]
+    const params=new URLSearchParams(t); //pass string in it 
     const token=params.get('token');
 
     const userId=verifyToken(token as string)
-    console.log(userId);
-    
+
+    if(userId==null){
+        socket.close()
+        return
+    }
+
+    localDB.push({
+        userId:userId,
+        rooms:[],
+        socket
+    })
 
 
 
-    
-     
-     
-    
-    
-    
-    //only logged in user can go forward else return 
+
     
 
 console.log(`listening ws on ${port}`);
 
         socket.on("message",(data)=>{
-            socket.send("dawg");
+            
+            if(typeof data !="string"){
+                return
+            }
+            const parsedData=JSON.parse(data)
+
+            if(parsedData.type==="join_room"){
+                const user=localDB.find(x => x.userId==userId)
+                //@ts-ignore
+                user?.rooms.push(parsedData.roomId)
+            }
+
+            if(parsedData.type==="leave_room"){
+                const user=localDB.find(x => x.userId==userId)
+                if(!user){
+                    return
+                }
+                user.rooms = user.rooms.filter(room => room !== parsedData.roomId)
+            }
+
+            
         })
 
 })
--- apps/ws-backend/src/verify.ts
+++ apps/ws-backend/src/verify.ts
@@ -1,21 +1,23 @@
 import jwt from 'jsonwebtoken'
 import {JWT_SECRET} from '@repo/backend-common/config'
-import { Response } from 'express';
 export const verifyToken=(token:string)=>{
-
+       
     try {
-        
-    const decoded=jwt.verify(token, JWT_SECRET)
+         const decoded=jwt.verify(token, JWT_SECRET)
 
     if(typeof decoded==="string"){
+        return null
+    }
+
+    if(!decoded || !decoded.userId){
         return null
     }
     const userId=decoded.userId
     return userId
         
-    } catch (e) {
+    } 
+    catch (e) {
         console.log(e);
-        return
     }
-
+        
 }
