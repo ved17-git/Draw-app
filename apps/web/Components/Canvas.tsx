99ce1d14809164007a0fe0f484275430a2bc4fe6
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2025-09-03 20:17:55 +0530

    added shapes array

--- apps/web/Components/Canvas.tsx
+++ apps/web/Components/Canvas.tsx
@@ -1,79 +1,22 @@
 "use client"
 import React, { useRef, useEffect } from "react";
+import { initializeDrawing } from "../draw/draw";
 
-interface Shapes{
-type:"rect",
-x:number,
-y:number,
-height:number,
-width:number
-}
 
 
 function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const  existingShape:Shapes[]=[]
+
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;  
-    const ctx = canvas.getContext("2d");
-    if(!ctx){
+
+    if(!canvasRef.current){
       return
     }
+    initializeDrawing(canvasRef.current)
 
-    ctx.fillStyle="rgba(0,0,0)";
-    ctx.fillRect(0,0, canvas.width, canvas.height)
-
-
-    
-
-
-    let clicked = false;
-    let startX = 0;
-    let startY = 0;
-
-
-    // mousedown
-    canvas.addEventListener("mousedown", (e) => {
-      clicked = true;
-      startX = e.clientX;
-      startY = e.clientY;
-    });
-
-    // mouseup
-    canvas.addEventListener("mouseup", (e) => {
-      clicked = false;
-      const width=startX-e.clientX
-      const height=startY-e.clientY
-
-      existingShape.push({
-        type:"rect",
-        x:startX,
-        y:startY,
-        width:width,
-        height:height
-      })
-
-    });
-
-    // mousemove
-    canvas.addEventListener("mousemove", (e) => {
-      if (!clicked) return;
-      const currentX = e.clientX;
-      const currentY = e.clientY;
-      const width = currentX - startX;
-      const height = currentY - startY;
-
-      ctx?.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle="rgba(0,0,0)";
-      ctx.fillRect(0,0, canvas.width, canvas.height)
-      ctx?.strokeRect(startX, startY, width, height)
-      ctx.strokeStyle="rgba(255,255,255)"
-      
-    });
-  }, []);
+  }, [canvasRef]);
 
 
 
---
+++ apps/web/draw/draw.ts
@@ -0,0 +1,86 @@
+
+
+interface Shapes{
+type:"rect",
+x:number,
+y:number,
+height:number,
+width:number
+}
+
+
+export const initializeDrawing=(canvas:HTMLCanvasElement)=>{
+
+    const ctx = canvas.getContext("2d");
+
+    if(!ctx){
+        return
+    }
+
+   const existingShapes:Shapes[]=[]
+
+    ctx.fillStyle="rgba(0,0,0)";
+    ctx.fillRect(0,0, canvas.width, canvas.height)
+
+
+  
+    let clicked = false;
+    let startX = 0;
+    let startY = 0;
+
+
+    // mousedown
+    canvas.addEventListener("mousedown", (e) => {
+      clicked = true;
+      startX = e.clientX;
+      startY = e.clientY;
+    });
+
+    // mouseup
+    canvas.addEventListener("mouseup", (e) => {
+      clicked = false;
+      const width=startX-e.clientX
+      const height=startY-e.clientY
+
+      existingShapes.push({
+        type:"rect",
+        x:startX,
+        y:startY,
+        width:width,
+        height:height
+      })
+    });
+
+    // mousemove
+    canvas.addEventListener("mousemove", (e) => {
+      if (!clicked) return;
+      const currentX = e.clientX;
+      const currentY = e.clientY;
+      const width = currentX - startX;
+      const height = currentY - startY;
+      
+
+      clearCanvas(existingShapes, canvas, ctx)
+
+      ctx?.strokeRect(startX, startY, width, height)
+      ctx.strokeStyle="rgba(255,255,255)"
+      
+    });
+}
+
+
+const clearCanvas=(existingShapes:Shapes[], canvas:HTMLCanvasElement, ctx:CanvasRenderingContext2D)=>{
+
+      ctx?.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle="rgba(0,0,0)";
+      ctx.fillRect(0,0, canvas.width, canvas.height);
+
+      existingShapes.map((shape)=>{
+
+        if(shape.type==="rect"){
+            ctx.strokeStyle="rgba(255,255,255)"
+            ctx?.strokeRect(shape.x, shape.y, shape.width, shape.height)
+        }
+      })
+
+}
