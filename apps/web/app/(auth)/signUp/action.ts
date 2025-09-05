2e5d4b688ab8c134b1ed7f7bd989f0558cbd4009
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2025-09-05 18:57:52 +0530

    tweaks

--- apps/web/app/(auth)/signUp/action.ts
+++ apps/web/app/(auth)/signUp/action.ts
@@ -1,13 +1,5 @@
 "use server"
 
 export const signUp=async(formData:FormData)=>{
-
-const username=formData.get("username")
-const email=formData.get("email")
-const password=formData.get("password")
-
-
-//post signup
-
-
+//post signup action
 }
