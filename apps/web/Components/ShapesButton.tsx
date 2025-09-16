e677dba4d4d91dcf22cf30b28f6649259750e547
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2025-09-16 18:29:50 +0530

    added shapes selection

--- apps/web/Components/ShapesButton.tsx
+++ apps/web/Components/ShapesButton.tsx
@@ -1,21 +1,36 @@
 import React from "react";
 import {useState} from 'react'
+import { FaRegCircle } from "react-icons/fa";
+import { RiRectangleLine } from "react-icons/ri";
+import { IoTriangleOutline } from "react-icons/io5";
+
+
 
 
 function ShapesButton() {
    
-const [active, setActive]=useState<"circle" | "rectangle" | "triangle">("circle")
+const [currentShape, setCurrentShape]=useState<"circle" | "rectangle" | "triangle"> ("circle")
+
 
 
   return (
     <>
-      <div className="flex gap-4 p-2"> 
-        <button className={active=='circle' ? "bg-red-200 p-3 rounded-full" : "bg-gray-200 p-3 rounded-full" } 
-        onClick={()=>setActive("circle")}>
-        C
-        </button>
-        <button className="bg-gray-200 p-3 rounded-full" onClick={select}>R</button>
-        <button className="bg-gray-200 p-3 rounded-full" onClick={select}>T</button>
+      <div className="flex gap-4 p-2 fixed top-2"> 
+            <button className={currentShape=='circle' ? "bg-green-400 p-3 rounded-full scale-110" : "bg-gray-200 p-3 rounded-full" } 
+            onClick={()=>setCurrentShape("circle")}>
+            <FaRegCircle />
+            </button>
+
+            <button className={currentShape=='rectangle' ? "bg-green-400 p-3 rounded-full scale-110" : "bg-gray-200 p-3 rounded-full" } 
+            onClick={()=>setCurrentShape("rectangle")}>
+            <RiRectangleLine />
+            </button>
+
+            <button className={currentShape=='triangle' ? "bg-green-400 p-3 rounded-full scale-110" : "bg-gray-200 p-3 rounded-full" } 
+            onClick={()=>setCurrentShape("triangle")}>
+            <IoTriangleOutline />
+
+            </button>
       </div>
     </>
   );
