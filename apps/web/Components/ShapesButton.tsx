d428cddbfc5c8c8edddf1935a527ec27775f0962
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2025-09-17 14:25:34 +0530

    added css to select shapes

--- apps/web/Components/ShapesButton.tsx
+++ apps/web/Components/ShapesButton.tsx
@@ -16,17 +16,17 @@ const [currentShape, setCurrentShape]=useState<"circle" | "rectangle" | "eraser"
   return (
     <>
       <div className="flex gap-4 p-2 fixed top-2"> 
-            <button className={currentShape=='circle' ? "bg-green-400 p-3 rounded-full scale-110" : "bg-gray-200 p-3 rounded-full hover:scale-110 cursor-pointer transition-all ease-in-out" } 
+            <button className={currentShape=='circle' ? "bg-green-400 p-3 rounded-full scale-115" : "bg-gray-200 p-3 rounded-full hover:scale-110 cursor-pointer transition-all ease-in-out" } 
             onClick={()=>setCurrentShape("circle")}>
             <FaRegCircle />
             </button>
 
-            <button className={currentShape=='rectangle' ? "bg-green-400 p-3 rounded-full scale-110" : "bg-gray-200 p-3 rounded-full hover:scale-110 cursor-pointer transition-all ease-in-out" } 
+            <button className={currentShape=='rectangle' ? "bg-green-400 p-3 rounded-full scale-115" : "bg-gray-200 p-3 rounded-full hover:scale-110 cursor-pointer transition-all ease-in-out" } 
             onClick={()=>setCurrentShape("rectangle")}>
             <RiRectangleLine />
             </button>
 
-            <button className={currentShape=='eraser' ? "bg-green-400 p-3 rounded-full scale-110" : "bg-gray-200 p-3 rounded-full hover:scale-110 cursor-pointer transition-all ease-in-out" } 
+            <button className={currentShape=='eraser' ? "bg-green-400 p-3 rounded-full scale-115" : "bg-gray-200 p-3 rounded-full hover:scale-110 cursor-pointer transition-all ease-in-out" } 
             onClick={()=>setCurrentShape("eraser")}>
             <BsEraser />
 
